61964e8057319961a2df4745c50fb50f310c2e1c
Aarya BC <aaryabc@Aaryas-MacBook-Pro.local>
2017-01-29 23:18:16 -0500

    added some functionality such as viewing movies in the viewer

---
+++ FlicksMovie/Pods/Target Support Files/Pods-FlicksMovie/Pods-FlicksMovie-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FlicksMovieVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FlicksMovieVersionString[];
+
